e86e501674edf68fb0d1c69a9b759d7899860277
Tankasala25 <tankasala.usedu1@gmail.com>
2025-01-30 19:29:37 +0000

    strings

--- string_1.py
+++ string_1.py
@@ -1,14 +1,36 @@
-a='harry'
-print(a)
+# Single-line strings
+str1 = 'Hello, World!'
+str2 = "Python Programming"
 
-para="""exploring well-curated GitHub repositories can 
-significantly enhance your Python learning journey.
-"""
+# Multi-line strings
+str3 = '''This is a
+multi-line string.'''
+str4 = """Another way to
+write multi-line strings."""
 
-print(para)
+my_string = "Hello"
 
-print(a[0])
-# print(a[5])-throws an index error
+for char in my_string:
+    print(char)
 
-for character in para:
-    print(character)
+for i in range(len(my_string)):
+    print(my_string[i])
+ 
+for i in range(len(my_string)-1,-1,-1):
+    print(my_string[i])
+
+for i in my_string[::-1]:
+    print(i)
+
+my_string = "Python"
+print(my_string[0])  # Output: P
+print(my_string[-1]) # Output: n (last character)
+
+str1 = "Hello"
+str2 = "World"
+result = str1 + " " + str2
+print(result)  # Output: Hello World
+
+my_string = "Python Programming"
+print("Python" in my_string)  # Output: True
+print("Java" in my_string)    # Output: False
