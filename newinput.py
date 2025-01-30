9479c6ede6496876d57a891644dde3716b99c167
Tankasala25 <tankasala.usedu1@gmail.com>
2025-01-30 00:17:11 +0000

    taking input from user

--- newinput.py
+++ newinput.py
@@ -1,10 +1,4 @@
-#taking input from user
+
+
 a=input("enter name:")
 print(a)
-
-# conversion of input from string to integer
-b=int(input("please enter number1:"))
-c=int(input("please enter number2:"))
-print(b+c)
-
-
