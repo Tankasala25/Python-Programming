ae04d22481392f378f9c379c421e16cf044efb72
Tankasala25 <tankasala.usedu1@gmail.com>
2025-02-13 19:27:06 +0000

    updated basics

--- Basics/newinput.py
+++ Basics/newinput.py
@@ -1,4 +1,22 @@
 
+# 1. Basic String Input
 
-a=input("enter name:")
-print(a)
+name = input("Enter your name: ")
+print("Hello, " + name + "!")
+
+# 2. Integer Input
+
+age = int(input("Enter your age: "))
+print("You will be", age + 1, "years old next year.")
+
+# 3. Float Input
+height = float(input("Enter your height in meters: "))
+print("Your height is", height, "meters.")
+
+# 4. Taking Multiple Inputs (Space-Separated)
+x = input("Enter x: ")
+y=input("Enter y:")
+print("First word:", x)
+print("Second word:", y)
+
+
