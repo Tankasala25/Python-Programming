1c13657de616148ade8e414bf468a656f0fc08c7
Tankasala25 <tankasala.usedu1@gmail.com>
2025-02-13 19:18:25 +0000

    Latest

--- Basics/Hello.py
+++ Basics/Hello.py
@@ -1,2 +1,6 @@
 print("Hello World")
-print(print("Hello world"))
+
+'''📌 How it Works?
+print() → A built-in function that displays output on the screen.
+"Hello, World!" → The text we want to print.
+🎯 Output when you run the program:'''
