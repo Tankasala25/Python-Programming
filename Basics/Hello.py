90db87dcdfe11fff6838ecf1136e2f8958ae0654
Tankasala25 <tankasala.usedu1@gmail.com>
2025-02-13 19:19:39 +0000

    Updated Basics

--- Basics/Hello.py
+++ Basics/Hello.py
@@ -3,4 +3,6 @@ print("Hello World")
 '''📌 How it Works?
 print() → A built-in function that displays output on the screen.
 "Hello, World!" → The text we want to print.
-🎯 Output when you run the program:'''
+🎯 Output when you run the program:
+
+Hello World'''
