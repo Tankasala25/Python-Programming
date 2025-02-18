2e2262b052225841bc0247f4c422b94b4cbbaa61
Tankasala25 <tankasala.usedu1@gmail.com>
2025-02-18 16:44:58 +0000

    installed sqlalchemy and created database fro blog

---
+++ Blog/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+
+sqlalchemydatabase_url="sqllite:///./blog.db"
+
+engine=create_engine(sqlalchemydatabase_url,connect_args = {"check_same_thread": False})
+
+
+SessionLocal=sessionmaker(bind=engine,autocommit=False,autoflush=False)
+
+Base=declarative_base()
