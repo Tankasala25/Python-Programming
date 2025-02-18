193cf4451366fcb28d7288a3b4a622993eccb4cd
Tankasala25 <tankasala.usedu1@gmail.com>
2025-02-18 02:11:58 +0000

    added query parameters and path parameters

--- main.py
+++ main.py
@@ -1,13 +1,47 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+import uvicorn
 
 app=FastAPI()
 
-@app.get("/")
-def index():
-    return {"Hello world"}
+@app.get("/blog")
+def index(limit=10,published:bool=True,sort:Optional[str]=None):
+
+    if published:
+        return {"data":f"{limit} blogs from published list"}
+    else:
+        return {"data":f"{limit} blogs from list"}
+
+class Blog(BaseModel):
+        title:str
+        body:str
+        published:Optional[bool]
+
+    
+
+
+@app.post("/create/blog")
+def create(request:Blog):
+    return f"Blog Created with {request.title}"
+
+
+@app.get("/blog/unpublished")
+def unpublished():
+    return {"data":"Unpublished Data"}
+
+@app.get("/blog/{id}")
+def showBlog(id:int):
+    return {"data":id}
+
 
 
 
 @app.get("/about")
 def about():
     return {"about:about page"}
+
+
+
+# if __name__=="__main__":
+#      uvicorn.run(app,host="127.0.0.1",port=9000)
